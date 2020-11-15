0d300dc5487f0c64ff349253d00f4f24e473dcfc
Jana Khanji <jana.khanji@ieee.org>
2020-11-15 09:17:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards(array[], eventS){
+  for( let i=0; i< array.length; i++){
+    console.log(``)
+  }
+}
